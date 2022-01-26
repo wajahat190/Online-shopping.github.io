a5e740ef5f1ba28ba1bf7912948d752365b1d090
wajahat190 <98448748+wajahat190@users.noreply.github.com>
2022-01-26 12:23:53 +0500

    My online shopping website

---
+++ jssapp.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+
+
+  
